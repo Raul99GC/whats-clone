6964bd07cb3fc9f00a187e22185cf4b1f32fb798
Raul99GC <raulc99g@gmail.com>
2023-09-11 11:41:52 -0600

    agregando variables de entorno en en la configs de nextjs

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    STORAGE_BUCKET: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
